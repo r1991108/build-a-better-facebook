5b42ae982603787f928174e290a10c238de8ea75
BINGNAN, CHEN <59923237+r1991108@users.noreply.github.com>
2020-01-23 00:28:44 +0900

    build a better facebook
    1. build a database.
    2. create information that is posted after accessing successfully.
    3. check whether isUserValid(username, password) is true or false.
    4. if isUserValid(username, password) is true, console.log(newsFeed); if not, alert("sorry, wrong info").
    5. let browser ask user's name and password.
    6. run signIn(userNamePrompt, passwordPromp)

---
+++ script.js
@@ -0,0 +1,55 @@
+// build a database
+var database=[
+    {
+        username: "Ben",
+        password: "benben",
+    },
+    {
+        username: "Sally",
+        password: "123",
+    },
+    {
+        username: "Ingrid",
+        password: "777",
+    }
+];
+
+// create informations that are posted after accessing successfully
+var newsFeed =[
+    {
+        username: "Bobby",
+        timeline: "So tired.",
+    },
+    {
+        username: "Sally",
+        timeline: "Javascript is so cool!",
+    }
+];
+
+// to check whether isUserValid(username, password) is true or false
+function isUserValid(username, password){
+    for (var i=0; i<database.length; i++){
+        if(database[i].username === username &&
+            database[i].password === password){
+                return true;
+            }
+    }
+    return false;
+}
+
+// if isUserValid(username, password) is true, console.log(newsFeed); if not, alert("sorry, wrong info")
+function signIn(username, password) {
+    
+    if(isUserValid(username, password)){
+        console.log(newsFeed);
+    }else{
+        alert("Sorry, wrong username or password");
+    }
+ }
+
+//  let browser ask user's name and password
+ var userNamePrompt = prompt("What's your username?");
+ var passwordPrompt = prompt("What's your password?");
+
+//  run signIn(userNamePrompt, passwordPromp)
+ signIn(userNamePrompt,passwordPrompt)
